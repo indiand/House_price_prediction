b286d828bdd99c98c6b83db2143ed7871eca943a
Nitesh <niteshbhat23@gmail.com>
2026-01-19 00:56:57 +0530

    d

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import pickle
 import numpy as np
 import pandas as pd
